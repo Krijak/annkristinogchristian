ffa1433e8d108f88f51eb9cc9673b11713b97c76
Kristina Jakobsen <krijak@stud.ntnu.no>
2025-04-01 16:15:29 +0200

    ups

--- src/pages/Program.tsx
+++ src/pages/Program.tsx
@@ -62,51 +62,57 @@ const TimeLine = () => {
 const Program = () => {
   return (
     <PageWrapper>
-      <DayAndDate day="Lørdag" date="16 / 08 / 25" pl={{ xs: 6, sm: 6, lg: 0 }}>
-        BRYLLUPSDAGEN
-      </DayAndDate>
-      <Box
-        ml={{ xs: 6, sm: 6, lg: 0 }}
-        mr={{ xs: 6, sm: 6, lg: 0 }}
-        maxWidth={"500px"}
-      >
-        <Typography>
-          Velkommen til hagebryllup i{" "}
-          <Link
-            target="_blank"
-            href="https://www.google.com/maps/place/Sarpemyrveien+51,+1560+Larkollen/@59.5437826,10.3970255,119874m/data=!3m1!1e3!4m6!3m5!1s0x4646ac30434edf45:0x601079678cbb378!8m2!3d59.3227404!4d10.7066174!16s%2Fg%2F11crt8jv9d?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D"
-          >
-            Sarpemyrveien 51
-          </Link>{" "}
-          i Larkollen ☀️
-        </Typography>
-      </Box>
+      <Stack>
+        <DayAndDate
+          day="Lørdag"
+          date="16 / 08 / 25"
+          pl={{ xs: 6, sm: 6, lg: 0 }}
+        >
+          BRYLLUPSDAGEN
+        </DayAndDate>
+        <Box
+          ml={{ xs: 6, sm: 6, lg: 0 }}
+          mr={{ xs: 6, sm: 6, lg: 0 }}
+          maxWidth={"500px"}
+        >
+          <Typography>
+            Velkommen til hagebryllup i{" "}
+            <Link
+              target="_blank"
+              href="https://www.google.com/maps/place/Sarpemyrveien+51,+1560+Larkollen/@59.5437826,10.3970255,119874m/data=!3m1!1e3!4m6!3m5!1s0x4646ac30434edf45:0x601079678cbb378!8m2!3d59.3227404!4d10.7066174!16s%2Fg%2F11crt8jv9d?entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D"
+            >
+              Sarpemyrveien 51
+            </Link>{" "}
+            i Larkollen ☀️
+          </Typography>
+        </Box>
 
-      <Stack
-        sx={{ maxWidth: "800px", width: "auto" }}
-        mt={10}
-        flexDirection={"column"}
-        justifyContent={"center"}
-        gap={2}
-      >
-        <AnimatedImage alt="Hagen" src={Hage} />
-      </Stack>
-      <Box pt={4} display={{ xs: "none", sm: "none", lg: "flex" }}>
-        <TimeLine />
-      </Box>
+        <Stack
+          sx={{ maxWidth: "800px", width: "auto" }}
+          mt={10}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          gap={2}
+        >
+          <AnimatedImage alt="Hagen" src={Hage} />
+        </Stack>
+        <Box pt={4} display={{ xs: "none", sm: "none", lg: "flex" }}>
+          <TimeLine />
+        </Box>
 
-      <Stack
-        display={{ xs: "flex", sm: "flex", lg: "none" }}
-        maxWidth={"800px"}
-        width={"100%"}
-        flexWrap={"wrap"}
-        justifyContent={{ xs: "center", sm: "center", lg: "flex-start" }}
-        alignContent={{ xs: "center", sm: "center", lg: "flex-start" }}
-        flexDirection={{ xs: "column", sm: "column", lg: "row" }}
-        mt={7}
-        pb={0}
-      >
-        <TimeLine />
+        <Stack
+          display={{ xs: "flex", sm: "flex", lg: "none" }}
+          maxWidth={"800px"}
+          width={"100%"}
+          flexWrap={"wrap"}
+          justifyContent={{ xs: "center", sm: "center", lg: "flex-start" }}
+          alignContent={{ xs: "center", sm: "center", lg: "flex-start" }}
+          flexDirection={{ xs: "column", sm: "column", lg: "row" }}
+          mt={7}
+          pb={0}
+        >
+          <TimeLine />
+        </Stack>
       </Stack>
     </PageWrapper>
   );
